b3a39be499972b07cdf9ad767398218a02bdee0e
BrianZylich <brian.zylich@gmail.com>
2018-01-23 19:02:01 -0500

    Added help button, tabs when fullscreened, and an initial verison of a distribution, range slider for filtering based on score.

--- resources/assets/js/components/layouts/MainView.jsx
+++ resources/assets/js/components/layouts/MainView.jsx
@@ -15,6 +15,7 @@ import IconFullscreen from 'material-ui/svg-icons/navigation/fullscreen';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 const styles = {
 	root: {
@@ -47,6 +48,8 @@ export default class MainView extends Component {
 		this.setState((prevState, props) => {
 			return {
 				colOverview: prevState.colOverview == 4 ? 12 : 4,
+				colGalaxy: 4,
+				colProfile: 4,
 			}
 		})
 	}
@@ -55,6 +58,8 @@ export default class MainView extends Component {
 		this.setState((prevState, props) => {
 			return {
 				colGalaxy: prevState.colGalaxy == 4 ? 12 : 4,
+				colOverview: 4,
+				colProfile: 4,
 			}
 		})
 	}
@@ -63,6 +68,8 @@ export default class MainView extends Component {
 		this.setState((prevState, props) => {
 			return {
 				colProfile: prevState.colProfile == 4 ? 12 : 4,
+				colOverview: 4,
+				colGalaxy: 4,
 			}
 		})
 	}
@@ -70,11 +77,124 @@ export default class MainView extends Component {
 	render() {
 
 		return (
+			<div>
 				<Grid fluid style={{ marginTop: 25, height: 500 }}>
-        <Row>
-          <Col xs={6} md={this.state.colOverview} style={{ 
-						display: (this.state.colGalaxy == 4 && this.state.colProfile == 4) ? 'block' : 'none',
-					}}>
+					<Row>
+						<Col xs={6} md={this.state.colOverview} style={{ 
+										display: (this.state.colGalaxy == 4 && this.state.colProfile == 4) ? 'block' : 'none',
+									}}>
+										<GridTile
+											title='Overview'
+											titlePosition="top"
+											titleBackground="#D62261"
+											style={{
+												border: '1px solid #F0F0F0', 
+												boxSizing: 'border-box',
+												background: 'white'
+											}}
+											actionIcon={
+												<IconButton 
+													tooltip="Expand"
+													iconStyle={{ color: 'white' }}
+													tooltipPosition='bottom-left'
+													onClick={this.toggleFullscreenOverview}
+												>
+													<IconFullscreen />	
+												</IconButton>
+											}
+										>
+											<DndGraph 
+												nodes={this.props.nodes}
+												links={this.props.links}
+												width={this.props.width}
+												height={this.props.height} 
+												selectedDrug={this.props.selectedDrug}
+												onClickNode={this.props.onClickNode}
+												isFetching={this.props.isFetching}
+												filter={this.props.filter}
+												minScore={this.props.minScore}
+												maxScore={this.props.maxScore}
+											/>
+										</GridTile>
+						</Col>
+						<Col xs={6} md={this.state.colGalaxy} style={{
+							display: this.state.colOverview == 4 && this.state.colProfile == 4 ? 'block' : 'none'
+						}}
+						>
+							<GridTile
+								title='Galaxy View'
+								titlePosition="top"
+								actionIcon={ 
+									<div>
+										<IconButton 
+											tooltip="Expand"
+											iconStyle={{ color: 'white' }}
+											tooltipPosition='bottom-left'
+											onClick={this.toggleFullscreenGalaxy}
+										>
+										<IconFullscreen />	
+									</IconButton>
+										<TreeViewFilterContainer /> 
+									</div>
+								}
+								titleBackground="#1BACC0"
+								style={{
+									border: '1px solid #F0F0F0', 
+									boxSizing: 'border-box',
+									background: 'white',
+									// overflow: 'auto',
+									// marginTop: 75
+								}}
+								cols={this.state.colGalaxy}
+							>
+								<DndTreeContainer 
+									currentDrugs={this.props.currentDrugs}
+									filter={this.props.filter}
+									minScore={this.props.minScore}
+									maxScore={this.props.maxScore}
+									width={this.props.width}
+									height={this.props.height} 
+									onClickNode={this.props.showDetailNode}
+									onDeleteNode={this.props.deleteNode}
+									cols={this.state.colGalaxy}
+									selectedDrug={this.props.selectedDrug}
+								/>
+							</GridTile>
+						</Col>
+						<Col xs={6} md={this.state.colProfile} style={{ display: this.state.colGalaxy == 4 && this.state.colOverview == 4 ? 'block' : 'none'}}>
+							<GridTile
+								title={'Interaction profile for: ' + this.props.selectedDrug}
+								titlePosition="top"
+								titleBackground="#8C2DA8"
+								style={{
+									border: '1px solid #F0F0F0', 
+									boxSizing: 'border-box',
+									background: 'white'
+								}}
+								actionIcon={
+									<IconButton 
+										tooltip="Expand"
+										iconStyle={{ color: 'white' }}
+										tooltipPosition='bottom-left'
+										onClick={this.toggleFullscreenProfile}
+									>
+										<IconFullscreen />	
+									</IconButton>
+								}
+							>
+								<InteractionProfile 
+									mainDrug={this.props.selectedDrug} 
+									rules={this.props.currentDrugs.find(el => el[0] == this.props.selectedDrug)}
+								/>
+							</GridTile>
+						</Col>
+					</Row>
+			</Grid>
+			<Grid style={{position: 'fixed',
+							bottom: 0,
+							}}>
+				<Row>
+					<Col xs={6} md={4} style={{ display: (this.state.colGalaxy == 4 && this.state.colOverview == 4 && this.state.colProfile == 4) || this.state.colOverview == 12 ? 'none' : 'block'}}>
 						<GridTile
 							title='Overview'
 							titlePosition="top"
@@ -82,7 +202,8 @@ export default class MainView extends Component {
 							style={{
 								border: '1px solid #F0F0F0', 
 								boxSizing: 'border-box',
-								background: 'white'
+								background: 'white',
+								height: 50,
 							}}
 							actionIcon={
 								<IconButton 
@@ -95,25 +216,9 @@ export default class MainView extends Component {
 								</IconButton>
 							}
 						>
-							<DndGraph 
-								nodes={this.props.nodes}
-								links={this.props.links}
-								width={this.props.width}
-								height={this.props.height} 
-								selectedDrug={this.props.selectedDrug}
-								onClickNode={this.props.onClickNode}
-								isFetching={this.props.isFetching}
-								filter={this.props.filter}
-								minScore={this.props.minScore}
-								maxScore={this.props.maxScore}
-							/>
 						</GridTile>
-          </Col>
-
-					<Col xs={6} md={this.state.colGalaxy} style={{
-						display: this.state.colOverview == 4 && this.state.colProfile == 4 ? 'block' : 'none'
-					}}
-					>
+					</Col>
+					<Col xs={6} md={4} style={{ display: (this.state.colGalaxy == 4 && this.state.colOverview == 4 && this.state.colProfile == 4) || this.state.colGalaxy == 12 ? 'none' : 'block'}}>
 						<GridTile
 							title='Galaxy View'
 							titlePosition="top"
@@ -135,27 +240,15 @@ export default class MainView extends Component {
 								border: '1px solid #F0F0F0', 
 								boxSizing: 'border-box',
 								background: 'white',
+								height: 50,
 								// overflow: 'auto',
 								// marginTop: 75
 							}}
 							cols={this.state.colGalaxy}
 						>
-							<DndTreeContainer 
-								currentDrugs={this.props.currentDrugs}
-								filter={this.props.filter}
-								minScore={this.props.minScore}
-								maxScore={this.props.maxScore}
-								width={this.props.width}
-								height={this.props.height} 
-								onClickNode={this.props.showDetailNode}
-								onDeleteNode={this.props.deleteNode}
-								cols={this.state.colGalaxy}
-								selectedDrug={this.props.selectedDrug}
-							/>
 						</GridTile>
-          </Col>
-
-          <Col xs={6} md={this.state.colProfile} style={{ display: this.state.colGalaxy == 4 && this.state.colOverview == 4 ? 'block' : 'none'}}>
+					</Col>
+					<Col xs={6} md={4} style={{ display: (this.state.colGalaxy == 4 && this.state.colOverview == 4 && this.state.colProfile == 4) || this.state.colProfile == 12 ? 'none' : 'block'}}>
 						<GridTile
 							title={'Interaction profile for: ' + this.props.selectedDrug}
 							titlePosition="top"
@@ -163,7 +256,8 @@ export default class MainView extends Component {
 							style={{
 								border: '1px solid #F0F0F0', 
 								boxSizing: 'border-box',
-								background: 'white'
+								background: 'white',
+								height: 50,
 							}}
 							actionIcon={
 								<IconButton 
@@ -176,14 +270,11 @@ export default class MainView extends Component {
 								</IconButton>
 							}
 						>
-							<InteractionProfile 
-								mainDrug={this.props.selectedDrug} 
-								rules={this.props.currentDrugs.find(el => el[0] == this.props.selectedDrug)}
-							/>
 						</GridTile>
 					</Col>
-        </Row>
-      </Grid>
+				</Row>
+			</Grid>
+	 	</div>
 		)
 	}
 }
