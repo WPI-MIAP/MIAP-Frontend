36598db3229a93c2d7a9c00571d3c8c8bb4f4c59
Huy Tran <hqtran@wpi.edu>
2018-01-30 17:15:50 -0500

    edit bugs in interaction profile

--- resources/assets/js/components/modules/InteractionProfile.jsx
+++ resources/assets/js/components/modules/InteractionProfile.jsx
@@ -61,7 +61,7 @@ const InteractionProfile = ({ mainDrug = '', rules = ['', { drugs: [], rules: []
     let child = myTreeData[0].children[i]
 
     const interactions = rules[1].rules
-    .filter(rule => rule.Drug1.name == child.name || rule.Drug2.name == child.name)
+    .filter(rule => _.capitalize(rule.Drug1.name) == child.name || _.capitalize(rule.Drug2.name) == child.name)
     .map(rule => rule.ADR)
 
     child.children = interactions.map(el => ({ name: _.capitalize(el) }))
